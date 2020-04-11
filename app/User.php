eccb4bb9471c77d801c3500d9f6ac6373039c1ea
VishnuSuthar28 <vishvsuthar28@gmail.com>
2020-04-11 19:40:40 +0530

    initial commit

--- app/User.php
+++ app/User.php
@@ -11,6 +11,8 @@ class User extends Authenticatable
 {
     use HasApiTokens, Notifiable;
 
+    protected $table = 'user_details';
+
     /**
      * The attributes that are mass assignable.
      *
