51afafc2fb8e994c9aeed6069464f17aec6529c7
VishnuSuthar28 <vishvsuthar28@gmail.com>
2020-04-11 19:58:51 +0530

    Added require modal

---
+++ app/UpdateModal.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class UpdateModal extends Model
+{
+    protected $table = 'update_details';
+}
---
+++ app/VisitorContact.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class VisitorContact extends Model
+{
+    protected $table = 'tbl_visitor_contact';
+}
